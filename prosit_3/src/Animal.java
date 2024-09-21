50f2464f08c09ac4c3c98f0e185e488ff85f0188
ChedlyRebai <chedly.rebai123@gmail.com>
2024-09-21 10:19:49 +0100

    Refactor Animal class: Add searchAnimal method

--- prosit_3/src/Animal.java
+++ prosit_3/src/Animal.java
@@ -23,6 +23,12 @@ public class Animal {
         return "family:" + family + "\n name:" + name + "\nage:" + age + "\n mamal:" + (isMammal ? " yes" : " no");
     }
 
+
+    public int searchAnimal(Animal animal){
+        
+        return 0;
+    }
+
     public String getFamily() {
         return family;
     }
