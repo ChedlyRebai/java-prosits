0d4ead76feb1b786fc2535e3237baf12cb9a1193
ChedlyRebai <chedly.rebai123@gmail.com>
2024-09-21 10:38:54 +0100

    Refactor Main class: Add new animals to the zoo and search for lion

--- prosit_3/src/Main.java
+++ prosit_3/src/Main.java
@@ -10,6 +10,8 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         Animal lion = new Animal("lion", "Lion", 3, true);
         Animal tiger = new Animal("tiger", "Tiger", 4, true);
+        Animal elephant = new Animal("elephant", "Elephant", 10, true);
+        Animal giraffe = new Animal("giraffe", "Giraffe", 5, true);
         // System.out.println("enter a valid number and must be less than 25:");
         /*
          * while(!sc.hasNextInt() || sc.nextInt() > 25){
@@ -23,15 +25,15 @@ public class Main {
         Animal[] animals = {};
         Zoo myZoo = new Zoo(animals, zooName, city, nbrcages);
         System.out.println("zoo animals");
-        System.out.println(myZoo.addAnimal(tiger) ? "ee":"e" );
+        System.out.println(myZoo.addAnimal(tiger) ? "ee" : "e");
         myZoo.addAnimal(lion);
-        myZoo.addAnimal(lion);
-        myZoo.addAnimal(lion);
-        int index=myZoo.searchAnimal(tiger);
-        if(index==(-1)){
+        myZoo.addAnimal(elephant);
+        myZoo.addAnimal(giraffe);
+        int index = myZoo.searchAnimal(lion);
+        if (index == (-1)) {
             System.out.println("not found");
-        }else{
-            System.out.println("found at index:"+index);
+        } else {
+            System.out.println("found at index:" + index);
         }
 
         sc.close();
