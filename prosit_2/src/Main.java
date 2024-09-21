d61ab722e8438bf444b396f94325059617cbb884
ChedlyRebai <chedly.rebai123@gmail.com>
2024-09-21 10:10:48 +0100

    Refactor Main class: Add input validation for number of cages

--- prosit_2/src/Main.java
+++ prosit_2/src/Main.java
@@ -6,17 +6,30 @@ public class Main {
     public static void main(String[] args) {
         String zooName = "esprit";
         String city = "tunis";
-        int nbrcages = 0;
+        int nbrcages = -1;
         Scanner sc = new Scanner(System.in);
         Animal lion = new Animal("lion", "Lion", 3, true);
         Animal tiger = new Animal("tiger", "Tiger", 4, true);
-
-        while (!sc.hasNextInt() || sc.nextInt() > 25) {
-            System.out.println("enter a valid number and must be less than 25:");
-            sc.next();
+        System.out.println("enter a valid number and must be less than 25:");
+        
+    
+        while (true) {
+            if (!sc.hasNextInt()) {
+                System.out.println("Enter a valid number:");
+                sc.next();  
+            } else {
+                nbrcages = sc.nextInt();
+                if (nbrcages <= 25) {
+                    break; 
+                } else {
+                    System.out.println("The number must be less than or equal to 25.");
+                }
+            }
         }
-
-        nbrcages = sc.nextInt();
+        
+        System.out.println("Number of cages: " + nbrcages);
+        
+      
         Animal[] animals = { lion, tiger };
 
         Zoo myZoo = new Zoo(animals, zooName, city, nbrcages);
