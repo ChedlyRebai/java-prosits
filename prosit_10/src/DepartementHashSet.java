f96dd9441e6afd2a0583f5eacb161d90e6137c31
ChedlyRebai <chedly.rebai123@gmail.com>
2024-11-20 21:06:09 +0100

    Refactor DepartementHashSet methods for improved readability and functionality; clean up workspace.xml

--- prosit_10/src/DepartementHashSet.java
+++ prosit_10/src/DepartementHashSet.java
@@ -1,40 +1,53 @@
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 import java.util.TreeSet;
 
 @SuppressWarnings("rawtypes")
-public class DepartementHashSet implements IDepartement  {
-    Set<Departement> Departements =new HashSet<>();
+public class DepartementHashSet implements IDepartement {
+    Set<Departement> Departements = new HashSet<>();
 
     @Override
     public void ajouterDepartement(Object t) {
-     
+        this.Departements.add((Departement) t);
     }
 
     @Override
     public boolean rechercherDepartement(String nom) {
-     
+        if (this.Departements.contains(nom)) {
+            return true;
+        }
+        return false;
     }
 
     @Override
     public boolean rechercherDepartement(Object t) {
-     
+        Departement dep = (Departement) t;
+        if (this.Departements.contains(dep)) {
+            return true;
+        }
+        return false;
     }
 
     @Override
     public void supprimerDepartement(Object t) {
-     
+        this.Departements.remove(t);
     }
 
     @Override
     public void displayDepartement() {
-     
+        Iterator<Departement> iterator = this.Departements.iterator();
+
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
     }
 
     @Override
     public TreeSet trierDepartementById() {
-     
+        TreeSet newSet = (TreeSet) this.Departements;
+        return newSet;
     }
 
-    
 }
