9449bc093e791a84ff482427c0c7de03212b8f21
ChedlyRebai <chedly.rebai123@gmail.com>
2024-12-02 16:16:24 +0100

    Remove Departement and DepartementHashSet classes as part of refactoring

--- prosit_12/src/Main.java
+++ prosit_12/src/Main.java
@@ -2,60 +2,6 @@ import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
-        DepartementHashSet gesDepartement = new DepartementHashSet();
-
-        Departement depInformatique = new Departement(1, "Informatique", 10);
-        Departement depPhysique = new Departement(2, "Physique", 20);
-        Departement depChimie = new Departement(3, "Chimie", 25);
-        Departement depBiologie = new Departement(4, "Biologie", 30);
-        Departement depMathematique = new Departement(6, "Mathematiques", 15);
-        Departement depGeologie = new Departement(5, "Geologie", 35);
-
-
-        gesDepartement.ajouterDepartement(depChimie);
-        gesDepartement.ajouterDepartement(depMathematique);
-        gesDepartement.ajouterDepartement(depBiologie);
-        gesDepartement.ajouterDepartement(depGeologie);
-        gesDepartement.ajouterDepartement(depPhysique);
-        gesDepartement.ajouterDepartement(depInformatique);
-        gesDepartement.displayDepartement();
-
-
-
-        if (gesDepartement.rechercherDepartement(depMathematique)) {
-            System.out.println("Department exist \n");
-        } else {
-            System.out.println("Department n'existe pas \n");
-        }
-
-        String nomDep = "Informatique";
-        if (gesDepartement.rechercherDepartement(nomDep)) {
-            System.out.println("department " + nomDep + " exist \n");
-        } else {
-            System.out.println("department " + nomDep + " n'exist pas \n");
-        }
-
        
-        System.out.println(" New treeSet :\n");
-        TreeSet<Departement> newDepartments= gesDepartement.trierDepartementById();
-        newDepartments.forEach((element)->{
-            System.out.println(element);
-        });
-
-        gesDepartement.supprimerDepartement(depMathematique);
-        
-        
-
-        Employee e1=new Employee(0, "chedly", "informatique", "informatique", 12);
-        Employee e2 = new Employee(1, "malek", "hmidi", "Physique", 5);
-        Employee e3 = new Employee(2, "amal", "hamdi", "Chimie", 8);
-        Employee E4 = new Employee(3, "houssem", "hammami", "Biologie", 3);
-
-
-        AffectationHashMap Affectation=new AffectationHashMap();
-        Affectation.ajouterEmployeDepartement(e1, depGeologie);
-        Affectation.ajouterEmployeDepartement(e3, depBiologie);
-        Affectation.afficherEmployesEtDepartements();
-
     }
 }
