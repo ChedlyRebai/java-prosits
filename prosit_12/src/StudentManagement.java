d9776cf6c294b6167c5c738226854b3ebaf58e0e
ChedlyRebai <chedly.rebai123@gmail.com>
2024-12-03 13:34:14 +0100

    Implement Management interface methods in StudentManagement class

--- prosit_12/src/StudentManagement.java
+++ prosit_12/src/StudentManagement.java
@@ -1,3 +1,47 @@
-public class StudentManagement {
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public class StudentManagement implements Management {
+
+    @Override
+    public void displayStudents(List<Student> students, Consumer<Student> con) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'displayStudents'");
+    }
+
+    @Override
+    public void displayStudentsByFilter(List<Student> students, Predicate<Student> pre, Consumer<Student> con) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'displayStudentsByFilter'");
+    }
+
+    @Override
+    public String returnStudentsNames(List<Student> students, Function<Student, String> fun) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'returnStudentsNames'");
+    }
+
+    @Override
+    public Student createStudent(Supplier<Student> sup) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createStudent'");
+    }
+
+    @Override
+    public List<Student> sortStudentsById(List<Student> students, Comparator<Student> com) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'sortStudentsById'");
+    }
+
+    @Override
+    public Stream<Student> convertToStream(List<Student> students) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'convertToStream'");
+    }
     
 }
